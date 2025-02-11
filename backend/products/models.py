dcb3d22dceed8864a2e56785a800d998320af295
Parsa <pheydari33@yahoo.com>
2025-02-11 20:45:25 +0330

    Django Rest Framework Model Serializers

--- backend/products/models.py
+++ backend/products/models.py
@@ -7,3 +7,10 @@ class Product(models.Model):
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
 
+
+    @property
+    def sale_price(self):
+        return f"{float(self.price) * 0.8:.2f}"
+    
+    def get_discount(self):
+        return '122'
