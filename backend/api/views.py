b69b36b3b086e41ec9d1176a065fc4cb62e52fe8
Parsa <pheydari33@yahoo.com>
2025-02-11 14:35:47 +0330

    Create First Api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def api_home(requets, *args, **kwargs):
+    return JsonResponse({"message": "Hi there, this is your Django API response.!"})
+
+
+
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,10 +2,11 @@ import requests
 
 #endpoint = 'https://httpbin.org/status/200/'
 #endpoint = 'https://httpbin.org/anything'
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, params={"query": "Hello world"})
 print(get_response.text)
 print(get_response.status_code) 
+print(get_response.json()['message'])
 
 #print(get_response.json())
