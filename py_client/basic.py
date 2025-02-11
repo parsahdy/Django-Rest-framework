21f8c92ead5e94802c3bb79456a6f0e29d0a0f62
Parsa <pheydari33@yahoo.com>
2025-02-11 16:39:30 +0330

    Create First Api view

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,9 +4,9 @@ import requests
 #endpoint = 'https://httpbin.org/anything'
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, params={"query": "Hello world"})
-print(get_response.text)
-print(get_response.status_code) 
-print(get_response.json()['message'])
+get_response = requests.get(endpoint, params={"abc":123}, json={"query": "Hello world"})
+#print(get_response.text)
+#print(get_response.status_code) 
+print(get_response.json())
 
 #print(get_response.json())
