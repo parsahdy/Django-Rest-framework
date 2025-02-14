c7ae4372ff160df76139e9ca2098f3d08c263f52
Parsa <pheydari33@yahoo.com>
2025-02-14 15:10:28 +0330

    generic API views

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,10 +4,11 @@ import requests
 #endpoint = 'https://httpbin.org/anything'
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.post(endpoint, params={"abc":123}, json={"title": "Hello world"})
+get_response = requests.get(endpoint, params={"abc":123}, json={"title": "Abc123",
+                                                                 "content": "Hello world",
+                                                                 "price": 99})
 #print(get_response.headers)
 #print(get_response.text)
 #print(get_response.status_code) 
 print(get_response.json())
 
-#print(get_response.json())
