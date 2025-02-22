0e185fe6ea7bf650000a57f1cd621be87673494c
Parsa <pheydari33@yahoo.com>
2025-02-22 18:27:33 +0330

    URLs, Reverse, & Serializers

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 #/api/products/
 urlpatterns = [
-    path('', views.ProductListCreateAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
-    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
+    path('', views.ProductListCreateAPIView.as_view(), name='product-list'),
+    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name='product-edit'),
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view()),
 ]
